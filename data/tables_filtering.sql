487dea519d166d1495eaac8acdc007a5b991bca7
Anuar Santoyo <anuar.santoyoalum@campus.tu-berlin.de>
2026-03-11 12:27:32 +0100

    Added sql filtering code

---
+++ data/tables_filtering.sql
@@ -0,0 +1,38 @@
+/* Filtering of sessions table */
+with active_users as
+    (select user_id, count(session_id) as total_session from sessions where session_start >= '2023-01-05' group by user_id having count(session_id) > 7)
+
+select * from sessions where sessions.session_start >= '2023-01-05' and sessions.user_id in (select user_id from active_users);
+
+
+/* Filtering of users table */
+with active_users as
+    (select user_id, count(session_id) as total_session from sessions where session_start >= '2023-01-05' group by user_id having count(session_id) > 7)
+
+select * from users where users.user_id in (select user_id from active_users);
+
+
+/* Filtering of flights table */
+
+with active_users as
+    (select user_id, count(session_id) as total_session from sessions where session_start >= '2023-01-05' group by user_id having count(session_id) > 7),
+valid_trip_id as
+   (select sessions.trip_id from sessions where sessions.session_start >= '2023-01-05' and sessions.user_id in (select user_id from active_users))
+
+select * from flights where flights.trip_id in (select trip_id from valid_trip_id);
+
+/* Filtering of hotels table */
+
+with active_users as
+    (select user_id, count(session_id) as total_session from sessions where session_start >= '2023-01-05' group by user_id having count(session_id) > 7),
+valid_trip_id as
+   (select sessions.trip_id from sessions where sessions.session_start >= '2023-01-05' and sessions.user_id in (select user_id from active_users))
+
+select * from hotels where hotels.trip_id in (select trip_id from valid_trip_id);
+
+
+
+
+
+
+
